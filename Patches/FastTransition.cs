9169d27452a45c045266bfaf2cee4e76d1482a6d
d3xMachina <16732772+d3xMachina@users.noreply.github.com>
2025-03-08 20:03:05 +0100

    fast transition patch for Suikoden 2

--- Patches/FastTransition.cs
+++ Patches/FastTransition.cs
@@ -2,6 +2,7 @@
 extern alias GSD2;
 
 using HarmonyLib;
+using UnityEngine;
 
 namespace Suikoden_Fix.Patches;
 
@@ -9,19 +10,14 @@ namespace Suikoden_Fix.Patches;
 public enum TransitionState
 {
     None,
-    // Zone changes
-    FieldInit,
-    OpenMapMain,
-    Village01,
-    // Title menu
-    TitleMain,
-    // Loading screen
-    UILoadingAnim
+    Zone,
+    TitleMenu,
+    Loading
 }
 
 public class FastTransitionPatch
 {
-    static void ApplyTransitionFactor(ref float time)
+    static float ApplyTransitionFactor(float time)
     {
         var transition = ModComponent.Instance.transition;
 
@@ -29,21 +25,35 @@ public class FastTransitionPatch
         {
             var factor = 1f;
 
-            if (transition >= TransitionState.UILoadingAnim)
+            switch (transition)
             {
-                factor = Plugin.Config.LoadingTransitionFactor.Value;
-            }
-            else if (transition >= TransitionState.TitleMain)
-            {
-                factor = Plugin.Config.TitleMenuTransitionFactor.Value;
-            }
-            else if (transition >= TransitionState.FieldInit)
-            {
-                factor = Plugin.Config.ZoneTransitionFactor.Value;
+                case TransitionState.Zone:
+                    factor = Plugin.Config.ZoneTransitionFactor.Value;
+                    break;
+                case TransitionState.TitleMenu:
+                    factor = Plugin.Config.TitleMenuTransitionFactor.Value;
+                    break;
+                case TransitionState.Loading:
+                    factor = Plugin.Config.LoadingTransitionFactor.Value;
+                    break;
+                default:
+                    break;
             }
 
             time = factor > 0f ? time / factor : 0f;
         }
+
+        return time;
+    }
+
+    static int ApplyTransitionFactor(int timer)
+    {
+        return (int)Mathf.Round(ApplyTransitionFactor((float)timer));
+    }
+
+    static ushort ApplyTransitionFactor(ushort timer)
+    {
+        return (ushort)ApplyTransitionFactor((int)timer);
     }
 
     [HarmonyPatch(typeof(UIManager), nameof(UIManager.FadeIn), [typeof(float), typeof(Il2CppSystem.Action)])]
@@ -52,8 +62,8 @@ public class FastTransitionPatch
     {
         if (ModComponent.Instance.transition != TransitionState.None)
         {
-            ApplyTransitionFactor(ref time);
-            Plugin.Log.LogInfo($"FADE IN 1: {time}s");
+            time = ApplyTransitionFactor(time);
+            //Plugin.Log.LogInfo($"FadeIn1: {time}s");
         }
     }
 
@@ -63,8 +73,8 @@ public class FastTransitionPatch
     {
         if (ModComponent.Instance.transition != TransitionState.None)
         {
-            ApplyTransitionFactor(ref time);
-            //Plugin.Log.LogInfo($"FADE IN 2: {time}s");
+            time = ApplyTransitionFactor(time);
+            //Plugin.Log.LogInfo($"FadeIn2: {time}s");
         }
     }
 
@@ -74,8 +84,8 @@ public class FastTransitionPatch
     {
         if (ModComponent.Instance.transition != TransitionState.None)
         {
-            ApplyTransitionFactor(ref time);
-            //Plugin.Log.LogInfo($"FADE IN HALF: {time}s");
+            time = ApplyTransitionFactor(time);
+            //Plugin.Log.LogInfo($"FadeInHalf: {time}s");
         }
     }
 
@@ -85,8 +95,8 @@ public class FastTransitionPatch
     {
         if (ModComponent.Instance.transition != TransitionState.None)
         {
-            ApplyTransitionFactor(ref time);
-            //Plugin.Log.LogInfo($"FADE OUT: {time}s");
+            time = ApplyTransitionFactor(time);
+            //Plugin.Log.LogInfo($"FadeOut: {time}s");
         }
     }
 
@@ -96,42 +106,129 @@ public class FastTransitionPatch
     {
         if (ModComponent.Instance.transition != TransitionState.None)
         {
-            ApplyTransitionFactor(ref time);
-            //Plugin.Log.LogInfo($"FADEWHITE OUT: {time}s");
+            time = ApplyTransitionFactor(time);
+            //Plugin.Log.LogInfo($"FadeWhiteOut: {time}s");
         }
     }
+
+    [HarmonyPatch(typeof(GSD1.G1_BACK), nameof(GSD1.G1_BACK.FilterBlackIn))]
+    [HarmonyPrefix]
+    static void FilterBlackIn(int timer)
+    {
+        if (ModComponent.Instance.transition != TransitionState.None)
+        {
+            timer = ApplyTransitionFactor(timer);
+            //Plugin.Log.LogInfo($"FilterBlackIn: timer={timer}");
+        }
+    }
+
+    [HarmonyPatch(typeof(GSD1.G1_BACK), nameof(GSD1.G1_BACK.FilterBlackIn))]
+    [HarmonyPrefix]
+    static void FilterBlackOut(int timer)
+    {
+        if (ModComponent.Instance.transition != TransitionState.None)
+        {
+            timer = ApplyTransitionFactor(timer);
+            //Plugin.Log.LogInfo($"FilterBlackOut: timer={timer}");
+        }
+    }
+
+    [HarmonyPatch(typeof(GSD2.G2_BACK), nameof(GSD2.G2_BACK.FilterBlackIn))]
+    [HarmonyPrefix]
+    static void FilterBlackIn(int pri, ref ushort timer)
+    {
+        if (ModComponent.Instance.transition != TransitionState.None)
+        {
+            timer = ApplyTransitionFactor(timer);
+            //Plugin.Log.LogInfo($"FilterBlackIn: pri={pri} timer={timer}");
+        }
+    }
+
+    [HarmonyPatch(typeof(GSD2.G2_BACK), nameof(GSD2.G2_BACK.FilterBlackInHalf))]
+    [HarmonyPrefix]
+    static void FilterBlackInHalf(int pri, ref ushort timer)
+    {
+        if (ModComponent.Instance.transition != TransitionState.None)
+        {
+            timer = ApplyTransitionFactor(timer);
+            //Plugin.Log.LogInfo($"FilterBlackInHalf: pri={pri} timer={timer}");
+        }
+    }
+
+    [HarmonyPatch(typeof(GSD2.G2_BACK), nameof(GSD2.G2_BACK.FilterBlackOut))]
+    [HarmonyPrefix]
+    static void FilterBlackOut(int pri, ref ushort timer, bool isReserve)
+    {
+        if (ModComponent.Instance.transition != TransitionState.None)
+        {
+            timer = ApplyTransitionFactor(timer);
+            //Plugin.Log.LogInfo($"FilterBlackOut: pri={pri} timer={timer}");
+        }
+    }
+
+    [HarmonyPatch(typeof(GSD2.G2_BACK), nameof(GSD2.G2_BACK.FilterBlackOutHalf))]
+    [HarmonyPrefix]
+    static void FilterBlackOutHalf(int pri, ref ushort timer, bool isReserve)
+    {
+        if (ModComponent.Instance.transition != TransitionState.None)
+        {
+            timer = ApplyTransitionFactor(timer);
+            //Plugin.Log.LogInfo($"FilterBlackOutHalf: pri={pri} timer={timer}");
+        }
+    }
+
+    /*
+    [HarmonyPatch(typeof(GSD2.G2_BACK), nameof(GSD2.G2_BACK.FilterSet))]
+    [HarmonyPrefix]
+    static void FilterSet(byte rgb, ref byte timer, Color color)
+    {
+        Plugin.Log.LogInfo($"FilterSet: rgb={rgb} timer={timer} color={color}");
+    }
+
+    [HarmonyPatch(typeof(GSD2.G2_BACK), nameof(GSD2.G2_BACK.FilterSetRGB))]
+    [HarmonyPrefix]
+    static void FilterSetRGB(byte rgb, ref ushort timer)
+    {
+        Plugin.Log.LogInfo($"FilterSetRGB: rgb={rgb} timer={timer}");
+    }
+
+    [HarmonyPatch(typeof(GSD2.G2_BACK), nameof(GSD2.G2_BACK.BackPolySetRGB))]
+    [HarmonyPrefix]
+    static void BackPolySetRGB(byte rgb, ref int timer)
+    {
+        Plugin.Log.LogInfo($"BackPolySetRGB: rgb={rgb} timer={timer}");
+    }
+    */
 }
 
 public class FastZoneTransitionPatch
 {
     [HarmonyPatch(typeof(GSD1::Fmain_c), nameof(GSD1::Fmain_c.fieldInit))]
-    [HarmonyPrefix]
-    static void GSD1_fieldInit()
-    {
-        ModComponent.Instance.transition = TransitionState.FieldInit;
-    }
-
     [HarmonyPatch(typeof(GSD1::Village_c), nameof(GSD1::Village_c.village01))]
-    [HarmonyPrefix]
-    static void GSD1_village01()
-    {
-        ModComponent.Instance.transition = TransitionState.Village01;
-    }
-
     [HarmonyPatch(typeof(GSD1::Village_c), nameof(GSD1::Village_c.open_map_main))]
+    [HarmonyPatch(typeof(GSD2::MACHICON), nameof(GSD2::MACHICON.MachiMain))]
+    [HarmonyPatch(typeof(GSD2::BattleManager), nameof(GSD2::BattleManager.exit_sub99))]
+    [HarmonyPatch(typeof(GSD2::BattleManager), nameof(GSD2::BattleManager.nige_window))]
+    [HarmonyPatch(typeof(GSD2::BattleManager), nameof(GSD2::BattleManager.okane_window))]
     [HarmonyPrefix]
-    static void GSD1_OpenMapMain()
+    static void Zone()
     {
-        ModComponent.Instance.transition = TransitionState.OpenMapMain;
+        ModComponent.Instance.transition = TransitionState.Zone;
+        //Plugin.Log.LogInfo("Zone");
     }
 
     [HarmonyPatch(typeof(GSD1::Fmain_c), nameof(GSD1::Fmain_c.fieldInit))]
     [HarmonyPatch(typeof(GSD1::Village_c), nameof(GSD1::Village_c.open_map_main))]
     [HarmonyPatch(typeof(GSD1::Village_c), nameof(GSD1::Village_c.village01))]
+    [HarmonyPatch(typeof(GSD2::MACHICON), nameof(GSD2::MACHICON.MachiMain))]
+    [HarmonyPatch(typeof(GSD2::BattleManager), nameof(GSD2::BattleManager.exit_sub99))]
+    [HarmonyPatch(typeof(GSD2::BattleManager), nameof(GSD2::BattleManager.nige_window))]
+    [HarmonyPatch(typeof(GSD2::BattleManager), nameof(GSD2::BattleManager.okane_window))]
     [HarmonyPostfix]
     static void ZonePost()
     {
         ModComponent.Instance.transition = TransitionState.None;
+        //Plugin.Log.LogInfo("Zone Post");
     }
 }
 
@@ -139,34 +236,42 @@ public class FastLoadingTransitionPatch
 {
     [HarmonyPatch(typeof(GSD1::UILoadingAnim._PlayGameStart_d__11), nameof(GSD1::UILoadingAnim._PlayGameStart_d__11.MoveNext))]
     [HarmonyPatch(typeof(GSD1::UILoadingAnim._PlayTitle_d__10), nameof(GSD1::UILoadingAnim._PlayTitle_d__10.MoveNext))]
+    [HarmonyPatch(typeof(GSD2::UILoadingAnim._Play_d__12), nameof(GSD2::UILoadingAnim._Play_d__12.MoveNext))]
     [HarmonyPrefix]
-    static void GSD1_UILoadingAnim()
+    static void Loading()
     {
-        ModComponent.Instance.transition = TransitionState.UILoadingAnim;
+        ModComponent.Instance.transition = TransitionState.Loading;
+        //Plugin.Log.LogInfo("Loading");
     }
 
     [HarmonyPatch(typeof(GSD1::UILoadingAnim._PlayGameStart_d__11), nameof(GSD1::UILoadingAnim._PlayGameStart_d__11.MoveNext))]
     [HarmonyPatch(typeof(GSD1::UILoadingAnim._PlayTitle_d__10), nameof(GSD1::UILoadingAnim._PlayTitle_d__10.MoveNext))]
+    [HarmonyPatch(typeof(GSD2::UILoadingAnim._Play_d__12), nameof(GSD2::UILoadingAnim._Play_d__12.MoveNext))]
     [HarmonyPostfix]
-    static void ZonePost()
+    static void LoadingPost()
     {
         ModComponent.Instance.transition = TransitionState.None;
+        //Plugin.Log.LogInfo("Loading Post");
     }
 }
 
 public class FastTitleMenuTransitionPatch
 {
     [HarmonyPatch(typeof(GSD1::TitleChapter), nameof(GSD1::TitleChapter.TitleMain))]
+    [HarmonyPatch(typeof(GSD2::TitleChapter), nameof(GSD2::TitleChapter.TitleMain))]
     [HarmonyPrefix]
-    static void GSD1_TitleMain()
+    static void TitleMenu()
     {
-        ModComponent.Instance.transition = TransitionState.TitleMain;
+        ModComponent.Instance.transition = TransitionState.TitleMenu;
+        //Plugin.Log.LogInfo("TitleMenu");
     }
 
     [HarmonyPatch(typeof(GSD1::TitleChapter), nameof(GSD1::TitleChapter.TitleMain))]
+    [HarmonyPatch(typeof(GSD2::TitleChapter), nameof(GSD2::TitleChapter.TitleMain))]
     [HarmonyPostfix]
-    static void ZonePost()
+    static void TitleMenuPost()
     {
         ModComponent.Instance.transition = TransitionState.None;
+        //Plugin.Log.LogInfo("TitleMenu Post");
     }
 }
