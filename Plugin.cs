2042b19fae5b0ae7f09cf305417ec33600319e30
d3xMachina <16732772+d3xMachina@users.noreply.github.com>
2025-03-10 18:29:34 +0100

    bump version

--- Plugin.cs
+++ Plugin.cs
@@ -7,7 +7,7 @@ using System;
 
 namespace Suikoden_Fix;
 
-[BepInPlugin("d3xMachina.suikoden_fix", "Suikoden Fix", "1.3.0")]
+[BepInPlugin("d3xMachina.suikoden_fix", "Suikoden Fix", "1.3.1")]
 public partial class Plugin : BasePlugin
 {
     public static new ManualLogSource Log;
