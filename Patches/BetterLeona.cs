de40fa399d4525ef82c65e396d1d8596fcc60f5d
d3xMachina <16732772+d3xMachina@users.noreply.github.com>
2025-05-04 14:08:12 +0200

    change the implementation of BetterLeona to enable other mods to modify PCpartyAllTblSet

--- Patches/BetterLeona.cs
+++ Patches/BetterLeona.cs
@@ -2,14 +2,27 @@
 extern alias GSD2;
 
 using HarmonyLib;
+using System;
 
 namespace Suikoden_Fix.Patches;
 
 public class BetterLeonaPatch
 {
+    private struct MemberCheck
+    {
+        public byte charaNo;
+        public bool check;
+
+        public MemberCheck(byte charaNo)
+        {
+            this.charaNo = charaNo;
+            this.check = true;
+        }
+    }
+
     [HarmonyPatch(typeof(GSD2.EventOverlayClass.Overlay_partychg), nameof(GSD2.EventOverlayClass.Overlay_partychg.PCpartyAllTblSet))]
-    [HarmonyPrefix]
-    static bool GSD2_SetPartyTable(GSD2.EventOverlayClass.Overlay_partychg.PCCON pcon)
+    [HarmonyPostfix]
+    static void GSD2_SetPartyTable(GSD2.EventOverlayClass.Overlay_partychg.PCCON pcon)
     {
         const int FlagRecruited = 4;
         const int IdValeria = 12;
@@ -17,82 +30,122 @@ public class BetterLeonaPatch
         const int IdMcDohl = 82;
 
         var ist = pcon?.ist;
-        if (ist == null || ist.Count < GSD2.EventOverlayClass.Overlay_partychg.PAMAX_MEM)
+        if (ist == null)
         {
-            return true;
+            return;
         }
 
         var gameWork = GSD2.GAME_WORK.Instance;
         if (gameWork == null)
         {
-            return true;
+            return;
         }
+
+        // Must be sorted by character ID in ascending order
+        var members = new MemberCheck[]
+        {
+            new(IdValeria),
+            new(IdKasumi),
+            new(IdMcDohl),
+        };
+
+        // McDohl ID is 82, that's why I added + 1
+        int maxMembers = Math.Max(ist.Count, GSD2.EventOverlayClass.Overlay_partychg.PAMAX_MEM + 1);
 
         var charaFlags = gameWork.chara_flag;
-        if (charaFlags == null || charaFlags.Count < GSD2.EventOverlayClass.Overlay_partychg.PAMAX_MEM + 1)
+        if (charaFlags == null || charaFlags.Count < maxMembers)
         {
-            return true;
+            return;
         }
 
-        // Handles loading pst array with the party members
-        GSD2.EventOverlayClass.Overlay_partychg.PCpartyTblSet(pcon);
+        var newIst = new GSD2.SP_PARTY[maxMembers];
+        int indexNewIst = 0;
 
-        int indexIst = 0;
-
-        // Add all available characters
-        // McDohl ID is 82, that's why I added + 1 and since index 0 is never used, no need to resize the ist array
-        for (int charaNo = 1; charaNo < GSD2.EventOverlayClass.Overlay_partychg.PAMAX_MEM + 1; ++charaNo)
+        void TryAddCharacters(byte currentCharaNo = byte.MaxValue)
         {
-            //Plugin.Log.LogWarning($"chano={charaNo} flags={charaFlags[charaNo]}");
-
-            var charaFlag = charaFlags[charaNo]; // Backup the character flag
-
-            if ((charaNo == IdMcDohl && gameWork.eventFlgCHK(0x41, 0x10)) || // McDohl
-                (charaNo == IdValeria && (charaFlags[IdKasumi] & FlagRecruited) != 0) || // Valeria
-                (charaNo == IdKasumi && (charaFlags[IdValeria] & FlagRecruited) != 0)) // Kasumi
+            for (int indexMember = 0; indexMember < members.Length; ++indexMember)
             {
-                charaFlags[charaNo] |= FlagRecruited; // Recruited
+                var member = members[indexMember];
+                if (!member.check || currentCharaNo < member.charaNo)
+                {
+                    continue;
+                }
+
+                members[indexMember].check = false;
+
+                // Character already added
+                if (currentCharaNo == member.charaNo)
+                {
+                    continue;
+                }
+
+                //Plugin.Log.LogWarning($"MOD Try Add {member.charaNo} currentCharaNo={currentCharaNo}");
+
+                // Try to add the character
+                var charaFlag = charaFlags[member.charaNo]; // Backup the character flag
+
+                if ((member.charaNo == IdMcDohl && gameWork.eventFlgCHK(0x41, 0x10)) || // McDohl
+                    (member.charaNo == IdValeria && (charaFlags[IdKasumi] & FlagRecruited) != 0) || // Valeria
+                    (member.charaNo == IdKasumi && (charaFlags[IdValeria] & FlagRecruited) != 0)) // Kasumi
+                {
+                    charaFlags[member.charaNo] |= FlagRecruited; // Recruited
+                }
+        
+                bool addCharacter = GSD2.G2_SYS.G2_cha_flag(4, member.charaNo) == 1 && // Check if character recruited, not dead or on leave, not in party and something else (bit 0x20 of charaFlags).
+                                    GSD2.G2_SYS.G2_cha_flag(9, member.charaNo) != 1; // Check if character in party. Shouldn't be necessary, looks like mode 4 checks for this already ?
+
+                charaFlags[member.charaNo] = charaFlag; // Restore the character flag
+
+                // Conditions not met
+                if (!addCharacter)
+                {
+                    continue;
+                }
+
+                //Plugin.Log.LogWarning($"MOD Add {member.charaNo}");
+
+                // Add the character
+                newIst[indexNewIst] = new()
+                {
+                    cno = member.charaNo,
+                    ist = 0,
+                    nou = 0
+                };
+
+                ++indexNewIst;
             }
-            
-            bool addCharacter = GSD2.G2_SYS.G2_cha_flag(4, charaNo) == 1 && // Check if character recruited, not dead or on leave, not in party and something else (bit 0x20 of charaFlags).
-                                GSD2.G2_SYS.G2_cha_flag(9, charaNo) != 1; // Check if character in party. Shouldn't be necessary, looks like mode 4 checks for this already ?
+        }
 
-            charaFlags[charaNo] = charaFlag; // Restore the character flag
-
-            if (!addCharacter)
+        // ist is sorted by character ID
+        for (int indexIst = 0; indexIst < pcon.inin; ++indexIst)
+        {
+            var spParty = ist[indexIst];
+            if (spParty == null)
             {
+                //Plugin.Log.LogWarning("SP_PARTY NULL");
                 continue;
             }
 
-            // Add the character
-            GSD2.SP_PARTY spParty = new()
-            {
-                cno = (byte)charaNo,
-                ist = 0,
-                nou = 0
-            };
+            // Try to insert the characters
+            TryAddCharacters(spParty.cno);
 
-            ist[indexIst] = spParty;
-            ++indexIst;
+            //Plugin.Log.LogWarning($"Add {spParty.cno}");
+            newIst[indexNewIst] = spParty;
+            ++indexNewIst;
         }
 
-        pcon.inin = indexIst;
-        if (indexIst == 0)
+        // Try to add the remaining characters
+        TryAddCharacters();
+
+        if (indexNewIst > 0)
         {
-            pcon.lpage = 0;
-            pcon.liby = 0;
-        }
-        else
-        {
-            pcon.lpage = (byte)((indexIst - 1) / GSD2.EventOverlayClass.Overlay_partychg.PWPARH);
-            pcon.liby = (byte)((indexIst - 1) % GSD2.EventOverlayClass.Overlay_partychg.PWPARH);
-        }
+            // Replace the characters array
+            pcon.ist = newIst;
+            pcon.inin = indexNewIst;
 
-        if (indexIst == 0 && pcon.pnin != GSD2.EventOverlayClass.Overlay_partychg.PWPARH)
-        {
-            --pcon.lpby;
+            // Recalculate the pagination
+            pcon.lpage = (byte)((indexNewIst - 1) / GSD2.EventOverlayClass.Overlay_partychg.PWPARH);
+            pcon.liby = (byte)((indexNewIst - 1) % GSD2.EventOverlayClass.Overlay_partychg.PWPARH);
         }
-
-        return false;
     }
 }
