0ba671031e38fe64b9303bc34947553f371897f9
d3xMachina <16732772+d3xMachina@users.noreply.github.com>
2025-05-07 14:44:12 +0200

    fix Valeria/Kasumi retrieved with BetterLeona having their equipment locked and not in richmond investigation

--- Patches/BetterLeona.cs
+++ Patches/BetterLeona.cs
@@ -8,6 +8,13 @@ namespace Suikoden_Fix.Patches;
 
 public class BetterLeonaPatch
 {
+    const int IdValeria = 12;
+    const int IdKasumi = 73;
+    const int IdMcDohl = 82;
+
+    private static bool _fakeRecruited = false;
+    private static bool _fakeRecruitedLeona = false;
+
     private struct MemberCheck
     {
         public byte charaNo;
@@ -24,19 +31,8 @@ public class BetterLeonaPatch
     [HarmonyPostfix]
     static void GSD2_SetPartyTable(GSD2.EventOverlayClass.Overlay_partychg.PCCON pcon)
     {
-        const int FlagRecruited = 4;
-        const int IdValeria = 12;
-        const int IdKasumi = 73;
-        const int IdMcDohl = 82;
-
         var ist = pcon?.ist;
         if (ist == null)
-        {
-            return;
-        }
-
-        var gameWork = GSD2.GAME_WORK.Instance;
-        if (gameWork == null)
         {
             return;
         }
@@ -51,13 +47,6 @@ public class BetterLeonaPatch
 
         // McDohl ID is 82, that's why I added + 1
         int maxMembers = Math.Max(ist.Count, GSD2.EventOverlayClass.Overlay_partychg.PAMAX_MEM + 1);
-
-        var charaFlags = gameWork.chara_flag;
-        if (charaFlags == null || charaFlags.Count < maxMembers)
-        {
-            return;
-        }
-
         var newIst = new GSD2.SP_PARTY[maxMembers];
         int indexNewIst = 0;
 
@@ -82,19 +71,12 @@ public class BetterLeonaPatch
                 //Plugin.Log.LogWarning($"MOD Try Add {member.charaNo} currentCharaNo={currentCharaNo}");
 
                 // Try to add the character
-                var charaFlag = charaFlags[member.charaNo]; // Backup the character flag
+                _fakeRecruitedLeona = true;
 
-                if ((member.charaNo == IdMcDohl && gameWork.eventFlgCHK(0x41, 0x10)) || // McDohl
-                    (member.charaNo == IdValeria && (charaFlags[IdKasumi] & FlagRecruited) != 0) || // Valeria
-                    (member.charaNo == IdKasumi && (charaFlags[IdValeria] & FlagRecruited) != 0)) // Kasumi
-                {
-                    charaFlags[member.charaNo] |= FlagRecruited; // Recruited
-                }
-        
                 bool addCharacter = GSD2.G2_SYS.G2_cha_flag(4, member.charaNo) == 1 && // Check if character recruited, not dead or on leave, not in party and something else (bit 0x20 of charaFlags).
                                     GSD2.G2_SYS.G2_cha_flag(9, member.charaNo) != 1; // Check if character in party. Shouldn't be necessary, looks like mode 4 checks for this already ?
-
-                charaFlags[member.charaNo] = charaFlag; // Restore the character flag
+                
+                _fakeRecruitedLeona = false;
 
                 // Conditions not met
                 if (!addCharacter)
@@ -147,5 +129,102 @@ public class BetterLeonaPatch
             pcon.lpage = (byte)((indexNewIst - 1) / GSD2.EventOverlayClass.Overlay_partychg.PWPARH);
             pcon.liby = (byte)((indexNewIst - 1) % GSD2.EventOverlayClass.Overlay_partychg.PWPARH);
         }
+    }
+
+    // Shops
+    [HarmonyPatch(typeof(GSD2.EventOverlayClass.hmonsyo), nameof(GSD2.EventOverlayClass.hmonsyo.hshopmonsyoinit))]
+    [HarmonyPatch(typeof(GSD2.EventOverlayClass.h_kaji), nameof(GSD2.EventOverlayClass.h_kaji.hshopkajiinit))]
+    [HarmonyPatch(typeof(GSD2.EventOverlayClass.h_dougu), nameof(GSD2.EventOverlayClass.h_dougu.hshopdouguinit))]
+    [HarmonyPatch(typeof(GSD2.EventOverlayClass.h_souko), nameof(GSD2.EventOverlayClass.h_souko.SoukoInit))]
+    [HarmonyPatch(typeof(GSD2.koueki), nameof(GSD2.koueki.ShopKouekiInit))]
+
+    // Richmond investigation
+    [HarmonyPatch(typeof(GSD2.EventOverlayClass.t_main), nameof(GSD2.EventOverlayClass.t_main.TanteiInit))]
+    [HarmonyPatch(typeof(GSD2.EventOverlayClass.t_main), nameof(GSD2.EventOverlayClass.t_main.CheckHintChar))]
+    [HarmonyPatch(typeof(GSD2.EventOverlayClass.t_phase), nameof(GSD2.EventOverlayClass.t_phase.TanteiPhaseInit))]
+    [HarmonyPatch(typeof(GSD2.EventOverlayClass.t_phase), nameof(GSD2.EventOverlayClass.t_phase.CheckDamyFaceChar))]
+    [HarmonyPatch(typeof(GSD2.EventOverlayClass.t_phase), nameof(GSD2.EventOverlayClass.t_phase.InitLvWindow))]
+    [HarmonyPatch(typeof(GSD2.EventOverlayClass.t_phase), nameof(GSD2.EventOverlayClass.t_phase.TanteiSecretInit))]
+    [HarmonyPatch(typeof(GSD2.EventOverlayClass.t_phase), nameof(GSD2.EventOverlayClass.t_phase.CheckSecretOpenLv4))]
+    
+    // Shops, warehouse, item transfers...
+    [HarmonyPatch(typeof(GSD2.G2_SYS), nameof(GSD2.G2_SYS.G2_eqp_chk))]
+    [HarmonyPatch(typeof(GSD2.G2_SYS), nameof(GSD2.G2_SYS.G2_item_num2))]
+
+    // Other
+    [HarmonyPatch(typeof(GSD2.fcommand), nameof(GSD2.fcommand.FCGetCharaPower))]
+    [HarmonyPrefix]
+    static void GSD2_FakeRecruited()
+    {
+        _fakeRecruited = true;
+    }
+
+    [HarmonyPatch(typeof(GSD2.EventOverlayClass.hmonsyo), nameof(GSD2.EventOverlayClass.hmonsyo.hshopmonsyoinit))]
+    [HarmonyPatch(typeof(GSD2.EventOverlayClass.h_kaji), nameof(GSD2.EventOverlayClass.h_kaji.hshopkajiinit))]
+    [HarmonyPatch(typeof(GSD2.EventOverlayClass.h_dougu), nameof(GSD2.EventOverlayClass.h_dougu.hshopdouguinit))]
+    [HarmonyPatch(typeof(GSD2.EventOverlayClass.h_souko), nameof(GSD2.EventOverlayClass.h_souko.SoukoInit))]
+    [HarmonyPatch(typeof(GSD2.koueki), nameof(GSD2.koueki.ShopKouekiInit))]
+    [HarmonyPatch(typeof(GSD2.EventOverlayClass.t_main), nameof(GSD2.EventOverlayClass.t_main.TanteiInit))]
+    [HarmonyPatch(typeof(GSD2.EventOverlayClass.t_main), nameof(GSD2.EventOverlayClass.t_main.CheckHintChar))]
+    [HarmonyPatch(typeof(GSD2.EventOverlayClass.t_phase), nameof(GSD2.EventOverlayClass.t_phase.TanteiPhaseInit))]
+    [HarmonyPatch(typeof(GSD2.EventOverlayClass.t_phase), nameof(GSD2.EventOverlayClass.t_phase.CheckDamyFaceChar))]
+    [HarmonyPatch(typeof(GSD2.EventOverlayClass.t_phase), nameof(GSD2.EventOverlayClass.t_phase.InitLvWindow))]
+    [HarmonyPatch(typeof(GSD2.EventOverlayClass.t_phase), nameof(GSD2.EventOverlayClass.t_phase.TanteiSecretInit))]
+    [HarmonyPatch(typeof(GSD2.EventOverlayClass.t_phase), nameof(GSD2.EventOverlayClass.t_phase.CheckSecretOpenLv4))]
+    [HarmonyPatch(typeof(GSD2.G2_SYS), nameof(GSD2.G2_SYS.G2_eqp_chk))]
+    [HarmonyPatch(typeof(GSD2.G2_SYS), nameof(GSD2.G2_SYS.G2_item_num2))]
+    [HarmonyPatch(typeof(GSD2.fcommand), nameof(GSD2.fcommand.FCGetCharaPower))]
+    [HarmonyPostfix]
+    static void GSD2_FakeRecruitedPost()
+    {
+        _fakeRecruited = false;
+    }
+
+    [HarmonyPatch(typeof(GSD2.G2_SYS), nameof(GSD2.G2_SYS.G2_cha_flag))]
+    [HarmonyPrefix]
+    static void GSD2_CharacterFlag(int chano, out int __state)
+    {
+        const int FlagRecruited = 4;
+        const int FlagManualRecruit = 70;
+
+        __state = -1;
+
+        if (!_fakeRecruitedLeona && (!_fakeRecruited || chano == IdMcDohl))
+        {
+            return;
+        }
+
+        var gameWork = GSD2.GAME_WORK.Instance;
+        if (gameWork == null)
+        {
+            return;
+        }
+
+        var charaFlags = gameWork.chara_flag;
+        if (charaFlags == null || IdMcDohl >= charaFlags.Count)
+        {
+            return;
+        }
+
+        if ((chano == IdMcDohl && gameWork.eventFlgCHK(0x41, 0x10)) || // McDohl
+            (chano == IdValeria && (charaFlags[IdKasumi] & FlagRecruited) != 0) || // Valeria
+            (chano == IdKasumi && (charaFlags[IdValeria] & FlagRecruited) != 0)) // Kasumi
+        {
+            __state = charaFlags[chano];
+            charaFlags[chano] = FlagManualRecruit;
+        }
+    }
+
+    [HarmonyPatch(typeof(GSD2.G2_SYS), nameof(GSD2.G2_SYS.G2_cha_flag))]
+    [HarmonyPostfix]
+    static void GSD2_CharacterFlagPost(int chano, int __state)
+    {
+        if (__state == -1)
+        {
+            return;
+        }
+
+        var charaFlags = GSD2.GAME_WORK.Instance.chara_flag;
+        charaFlags[chano] = (byte)__state;
     }
 }
